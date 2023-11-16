ab110bd67d42f144f8f72ea69b4e2bc57c91c6d3
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-11-16 20:20:07 +0900

    add: 이메일 유효성 검사의 응답 결과 JSON에 대한 모델 생성

---
+++ MementoMori/Model/EmailValidationModel.swift
@@ -0,0 +1,12 @@
+//
+//  EmailValidationModel.swift
+//  MementoMori
+//
+//  Created by Taekwon Lee on 2023/11/16.
+//
+
+import Foundation
+
+struct EmailValidationModel: Codable {
+    let message: String
+}
