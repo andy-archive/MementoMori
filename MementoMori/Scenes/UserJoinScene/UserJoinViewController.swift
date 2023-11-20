8e5b9cfad9855aa88bcfcff575b5674b99d07405
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-11-20 17:41:46 +0900

    feat: 선택 입력 사항(번호, 생년월일)에 대한 UI 추가

--- MementoMori/Scenes/UserJoinScene/UserJoinViewController.swift
+++ MementoMori/Scenes/UserJoinScene/UserJoinViewController.swift
@@ -13,14 +13,18 @@ import RxSwift
 final class UserJoinViewController: BaseViewController {
     
     private lazy var titleLabel = SigninTitleLabel()
-    private lazy var subtitleLabel = SigninSubtitleLabel()
+    private lazy var requiredSubtitleLabel = SigninSubtitleLabel()
     private lazy var emailTextField = SigninTextField()
     private lazy var emailValidationButton = SigninButton()
     private lazy var emailValidationLabel = SigninSubtitleLabel()
     private lazy var passwordTextField = SigninTextField()
     private lazy var passwordValidationLabel = SigninSubtitleLabel()
+    private lazy var passwordSecureTextButton = SecureTextButton()
     private lazy var nicknameTextField = SigninTextField()
-    private lazy var secureTextButton = SecureTextButton()
+    private lazy var selectiveSubtitleLabel = SigninSubtitleLabel()
+    private lazy var phoneNumberTextField = SigninTextField()
+    private lazy var birthdayTextField = SigninTextField()
+    private lazy var nextButton = SigninButton()
     
     private let viewModel = UserJoinViewModel()
     private let disposeBag = DisposeBag()
@@ -66,7 +70,8 @@ final class UserJoinViewController: BaseViewController {
         nicknameTextField.delegate = self
         
         titleLabel.text = "회원 가입"
-        subtitleLabel.text = "필수 입력 사항"
+        
+        requiredSubtitleLabel.text = "필수 입력 사항"
         emailTextField.placeholder = "📧 이메일"
         emailTextField.keyboardType = .emailAddress
         emailTextField.autocapitalizationType = .none
@@ -74,22 +79,35 @@ final class UserJoinViewController: BaseViewController {
         emailTextField.becomeFirstResponder()
         emailTextField.rightViewMode = .always
         emailTextField.rightView = emailValidationButton
+        emailValidationButton.setTitle("확인", for: .normal)
         passwordTextField.placeholder = "🔒 비밀번호"
         passwordTextField.isSecureTextEntry = true
         passwordTextField.returnKeyType = .continue
         passwordTextField.rightViewMode = .always
-        passwordTextField.rightView = secureTextButton
+        passwordTextField.rightView = passwordSecureTextButton
         nicknameTextField.placeholder = "🔖 닉네임"
-        emailValidationButton.setTitle("확인", for: .normal)
+        nicknameTextField.returnKeyType = .continue
+        
+        selectiveSubtitleLabel.text = "선택 입력 사항"
+        phoneNumberTextField.placeholder = "📱 휴대전화번호"
+        phoneNumberTextField.keyboardType = .numberPad
+        birthdayTextField.placeholder = "📅 생년월일 8자리"
+        birthdayTextField.keyboardType = .numberPad
+        nextButton.setTitle("다음 ", for: .normal)
+        nextButton.titleLabel?.font = .boldSystemFont(ofSize: Constant.FontSize.title)
+        nextButton.layer.cornerRadius = 15
         
         view.addSubview(titleLabel)
-        view.addSubview(subtitleLabel)
+        view.addSubview(requiredSubtitleLabel)
         view.addSubview(emailTextField)
         view.addSubview(emailValidationLabel)
         view.addSubview(passwordTextField)
         view.addSubview(passwordValidationLabel)
         view.addSubview(nicknameTextField)
-        view.addSubview(emailValidationButton)
+        view.addSubview(selectiveSubtitleLabel)
+        view.addSubview(phoneNumberTextField)
+        view.addSubview(birthdayTextField)
+        view.addSubview(nextButton)
     }
     
     override func configureLayout() {
@@ -102,16 +120,16 @@ final class UserJoinViewController: BaseViewController {
             titleLabel.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -Constant.Layout.UserAuth.Inset.horizontal)
         ])
         
-        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        requiredSubtitleLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            subtitleLabel.topAnchor.constraint(equalTo: titleLabel.safeAreaLayoutGuide.bottomAnchor, constant: Constant.Layout.UserAuth.Inset.vertical),
-            subtitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constant.Layout.UserAuth.Inset.horizontal),
-            subtitleLabel.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -Constant.Layout.UserAuth.Inset.horizontal)
+            requiredSubtitleLabel.topAnchor.constraint(equalTo: titleLabel.safeAreaLayoutGuide.bottomAnchor, constant: Constant.Layout.UserAuth.Inset.vertical),
+            requiredSubtitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constant.Layout.UserAuth.Inset.horizontal),
+            requiredSubtitleLabel.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -Constant.Layout.UserAuth.Inset.horizontal)
         ])
         
         emailTextField.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            emailTextField.topAnchor.constraint(equalTo: subtitleLabel.safeAreaLayoutGuide.bottomAnchor, constant: Constant.Layout.UserAuth.Inset.vertical),
+            emailTextField.topAnchor.constraint(equalTo: requiredSubtitleLabel.safeAreaLayoutGuide.bottomAnchor, constant: Constant.Layout.UserAuth.Inset.vertical / 2),
             emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constant.Layout.UserAuth.Inset.horizontal),
             emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constant.Layout.UserAuth.Inset.horizontal),
             emailTextField.heightAnchor.constraint(equalToConstant: Constant.Layout.UserAuth.Size.height)
@@ -146,6 +164,37 @@ final class UserJoinViewController: BaseViewController {
             nicknameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constant.Layout.UserAuth.Inset.horizontal),
             nicknameTextField.heightAnchor.constraint(equalToConstant: Constant.Layout.UserAuth.Size.height)
         ])
+        
+        selectiveSubtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            selectiveSubtitleLabel.topAnchor.constraint(equalTo: nicknameTextField.safeAreaLayoutGuide.bottomAnchor, constant: Constant.Layout.UserAuth.Inset.vertical),
+            selectiveSubtitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constant.Layout.UserAuth.Inset.horizontal),
+            selectiveSubtitleLabel.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -Constant.Layout.UserAuth.Inset.horizontal)
+        ])
+        
+        phoneNumberTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            phoneNumberTextField.topAnchor.constraint(equalTo: selectiveSubtitleLabel.safeAreaLayoutGuide.bottomAnchor, constant: Constant.Layout.UserAuth.Inset.vertical / 2),
+            phoneNumberTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constant.Layout.UserAuth.Inset.horizontal),
+            phoneNumberTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constant.Layout.UserAuth.Inset.horizontal),
+            phoneNumberTextField.heightAnchor.constraint(equalToConstant: Constant.Layout.UserAuth.Size.height)
+        ])
+        
+        birthdayTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            birthdayTextField.topAnchor.constraint(equalTo: phoneNumberTextField.safeAreaLayoutGuide.bottomAnchor, constant: Constant.Layout.UserAuth.Inset.vertical),
+            birthdayTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constant.Layout.UserAuth.Inset.horizontal),
+            birthdayTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constant.Layout.UserAuth.Inset.horizontal),
+            birthdayTextField.heightAnchor.constraint(equalToConstant: Constant.Layout.UserAuth.Size.height)
+        ])
+        
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            nextButton.topAnchor.constraint(equalTo: birthdayTextField.safeAreaLayoutGuide.bottomAnchor, constant: Constant.Layout.UserAuth.Inset.vertical),
+            nextButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constant.Layout.UserAuth.Inset.horizontal),
+            nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constant.Layout.UserAuth.Inset.horizontal),
+            nextButton.heightAnchor.constraint(equalToConstant: Constant.Layout.UserAuth.Size.height)
+        ])
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -161,6 +210,7 @@ extension UserJoinViewController: UITextFieldDelegate {
         switch textField {
         case emailTextField: passwordTextField.becomeFirstResponder()
         case passwordTextField: nicknameTextField.becomeFirstResponder()
+        case nicknameTextField: phoneNumberTextField.becomeFirstResponder()
         default: break
         }
         
