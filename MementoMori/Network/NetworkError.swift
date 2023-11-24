69fe0ca01943123bc1eb13a38061eb5e4d0d84b0
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-11-24 13:21:52 +0900

    refactor: 회원 가입에 대한 네트워크 에러 추가

--- MementoMori/Network/NetworkError.swift
+++ MementoMori/Network/NetworkError.swift
@@ -7,15 +7,22 @@
 
 import Foundation
 
+protocol ReusableError: Error {
+    var rawValue: Int { get }
+    var message: String { get }
+}
+
 enum NetworkError: Int, Error {
+    case badRequest = 400
+    case conflict = 409
     case notValidKey = 420
     case tooManyRequests = 429
     case noResponse = 444
     case internalServerError = 500
 }
 
-extension NetworkError: LocalizedError {
-    private var message: String {
+extension NetworkError: LocalizedError, ReusableError {
+    internal var message: String {
         switch self {
         case .notValidKey:
             return "키가 적절하지 않습니다."
@@ -25,6 +32,22 @@ extension NetworkError: LocalizedError {
             return "응답 없음"
         case .internalServerError:
             return "서버 에러"
+        default:
+            return ""
+        }
+    }
+}
+
+enum SignupValidationError: Int, ReusableError {
+    case badRequest = 400
+    case conflict = 409
+
+    var message: String {
+        switch self {
+        case .badRequest:
+            return "필수값을 채워주세요."
+        case .conflict:
+            return "이미 가입된 유저입니다."
         }
     }
 }
