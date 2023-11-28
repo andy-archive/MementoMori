4cdb6f391b9d41eae9a95fbdcbaea28f135526dd
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-11-28 16:21:37 +0900

    chore: 로그인 DTO 모델 생성 및 기존 Entities, DTO 리팩토링

--- MementoMori/Sources/Data/DataMapping/UserJoinDTO.swift
+++ MementoMori/Sources/Data/DataMapping/UserJoinDTO.swift
@@ -1,11 +1,19 @@
 //
-//  UserJoinResponseDTO.swift
+//  UserJoinDTO.swift
 //  MementoMori
 //
 //  Created by Taekwon Lee on 11/26/23.
 //
 
 import Foundation
+
+struct UserJoinRequestDTO: Encodable {
+    let email: String
+    let password: String
+    let nick: String
+    let phoneNum: String?
+    let birthday: String?
+}
 
 struct UserJoinResponseDTO: Decodable {
     let id: String
---
+++ MementoMori/Sources/Data/DataMapping/UserSigninDTO.swift
@@ -0,0 +1,26 @@
+//
+//  UserSigninDTO.swift
+//  MementoMori
+//
+//  Created by Taekwon Lee on 11/28/23.
+//
+
+import Foundation
+
+struct UserSigninRequestDTO: Encodable {
+    let email: String
+    let password: String
+}
+
+struct UserSigninResponseDTO: Decodable {
+    let id: String
+    let token: String
+    let refreshToken: String
+
+    func toDomain() -> Authorization {
+        return Authorization(
+            token: token,
+            refreshToken: refreshToken
+        )
+    }
+}
