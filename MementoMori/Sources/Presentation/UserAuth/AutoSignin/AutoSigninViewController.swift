e2a023bed4ff46f2b238744144fca21e63a07a3b
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-12-11 14:39:36 +0900

    add: AutoSignin - VC, Coordinator

---
+++ MementoMori/Sources/Presentation/UserAuth/AutoSignin/AutoSigninViewController.swift
@@ -0,0 +1,132 @@
+//
+//  AutoSigninViewController.swift
+//  MementoMori
+//
+//  Created by Taekwon Lee on 12/10/23.
+//
+
+import UIKit
+
+final class AutoSigninViewController: BaseViewController {
+    
+    private lazy var titleLabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Noteworthy", size: 35)
+        label.textColor = Constant.Color.label
+        label.numberOfLines = 1
+        label.text = "Memento Mori"
+        return label
+    }()
+    private lazy var nicknameLabel = {
+        let label = SigninSubtitleLabel()
+        return label
+    }()
+    private lazy var autoSigninButton = {
+        let button = SigninButton()
+        button.setTitle("로그인", for: .normal)
+        return button
+    }()
+    private lazy var otherSigninButton = {
+        let button = JoinButton()
+        button.setTitle("다른 계정으로 로그인", for: .normal)
+        return button
+    }()
+    private lazy var joinButton = {
+        let button = JoinButton()
+        button.setTitle("새 계정 만들기", for: .normal)
+        return button
+    }()
+    
+//    private let viewModel: AutoSigninViewModel
+//
+//    init(viewModel: AutoSigninViewModel) {
+//        self.viewModel = viewModel
+//        
+//        super.init()
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+//    override func bind() {
+//        let input =
+//        )
+//        
+//        let output = viewModel.transform(input: input)
+//    }
+    
+    override func configureUI() {
+        super.configureUI()
+        
+        view.addSubview(titleLabel)
+        view.addSubview(nicknameLabel)
+        view.addSubview(autoSigninButton)
+        view.addSubview(otherSigninButton)
+        view.addSubview(joinButton)
+    }
+    
+    override func configureLayout() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Constant.Layout.Common.Inset.vertical),
+            titleLabel.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: Constant.Layout.Common.Inset.horizontal),
+            titleLabel.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -Constant.Layout.Common.Inset.horizontal),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+        
+        nicknameLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            nicknameLabel.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: Constant.Layout.Common.Inset.horizontal),
+            nicknameLabel.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -Constant.Layout.Common.Inset.horizontal),
+            nicknameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nicknameLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+        
+        autoSigninButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            autoSigninButton.topAnchor.constraint(equalTo: nicknameLabel.safeAreaLayoutGuide.bottomAnchor, constant: Constant.Layout.Common.Inset.vertical),
+            autoSigninButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constant.Layout.Common.Inset.horizontal),
+            autoSigninButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constant.Layout.Common.Inset.horizontal),
+            autoSigninButton.heightAnchor.constraint(equalToConstant: Constant.Layout.Common.Size.buttonHeight)
+        ])
+        
+        otherSigninButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            otherSigninButton.topAnchor.constraint(equalTo: autoSigninButton.bottomAnchor, constant: Constant.Layout.Common.Inset.vertical / 2),
+            otherSigninButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constant.Layout.Common.Inset.horizontal),
+            otherSigninButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constant.Layout.Common.Inset.horizontal),
+            otherSigninButton.heightAnchor.constraint(equalToConstant: Constant.Layout.Common.Size.buttonHeight)
+        ])
+        
+        joinButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            joinButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constant.Layout.Common.Inset.horizontal),
+            joinButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constant.Layout.Common.Inset.horizontal),
+            joinButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -Constant.Layout.Common.Inset.vertical),
+            joinButton.heightAnchor.constraint(equalToConstant: Constant.Layout.Common.Size.buttonHeight)
+        ])
+    }
+}
+
+#if DEBUG
+import SwiftUI
+struct Preview: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> UIViewController {
+        AutoSigninViewController()
+    }
+    
+    func updateUIViewController(_ uiView: UIViewController,context: Context) {
+    }
+}
+
+struct ViewController_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            Preview()
+                .previewDisplayName("Preview")
+        }
+    }
+}
+#endif
